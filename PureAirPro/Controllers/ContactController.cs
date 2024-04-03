4e7d71ca27a0d1dd92005b9987cf3851d2bc05a7
nimisha <nimishanimmz@gmail.com>
2024-04-03 01:13:45 -0400

    Contact page design

---
+++ PureAirPro/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using PureAirPro.Models;
+using System.Diagnostics;
+
+namespace PureAirPro.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly ILogger<ContactController> _logger;
+
+        public ContactController(ILogger<ContactController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
