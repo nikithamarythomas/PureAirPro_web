ca8778cee887d69af353e7500449a5b57fd4068e
parth chaudhari <parth2372002@gmail.com>
2024-04-09 10:48:45 -0400

    created homepage,product page

---
+++ PureAirPro/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PureAirPro.Models;
+using System.Diagnostics;
+
+namespace PureAirPro.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+       
+
+        public IActionResult Product()
+        {
+            return View();
+        }
+
+
+       
+    }
+}
